b12bda191985464564f7fe2f31a0acf7ba9cec55
Napster13Nord <andre@wpexperts.pt>
2026-09-02 02:38:01 +0300

    chore: reejetado do motor (/12-oficina-auto/rbauto-ricardo-baptista-reparacao)

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,7 +2,7 @@ import './globals.css';
 import { CLASSES_FONTES } from '../lib/fontes';
 
 export const metadata = {
-  icons: { icon: '/logos/rbauto-ricardo-baptista-reparacao.png', shortcut: '/logos/rbauto-ricardo-baptista-reparacao.png', apple: '/logos/rbauto-ricardo-baptista-reparacao.png' },
+  icons: { icon: '/logos/rbauto-ricardo-baptista-reparacao-41405716.png', shortcut: '/logos/rbauto-ricardo-baptista-reparacao-41405716.png', apple: '/logos/rbauto-ricardo-baptista-reparacao-41405716.png' },
 };
 
 export default function RootLayout({ children }) {
